2774cb6d8cc231b8cc194ae08dcbdc8f6814aa3a
Pashatania Fitri Indah Lestari <87978506+pashatania1@users.noreply.github.com>
2024-08-19 13:47:48 +0700

    Add files via upload

---
+++ SiswaTester.java
@@ -0,0 +1,14 @@
+//Driver Class
+public class SiswaTester {
+    public static void main(String[] args) {
+        //membuat Object
+        //namaClass namaObject = new Constructor();
+        Siswa sabrina = new Siswa();
+        Siswa rasyid = new Siswa();
+
+        sabrina.id = 32;
+        sabrina.nama = "Sabrina";
+        rasyid.id = 30;
+        sabrina.print();
+    }
+}
