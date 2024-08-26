4eae26f788ad6b3d5d11d50b2ee8148853003331
Pashatania Fitri Indah Lestari <87978506+pashatania1@users.noreply.github.com>
2024-08-26 13:18:12 +0700

    Add files via upload

--- SiswaTester.java
+++ SiswaTester.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 //Driver Class
 public class SiswaTester {
     public static void main(String[] args) {
+        //String-->int--->double
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nama : ");
+        String nama = input.nextLine();
+        System.out.println("ID : ");
+        int id = input.nextInt();
+        System.out.println("IPK : ");
+        double ipk = input.nextDouble();
+        input.close();
+
+        Siswa dimas = new Siswa(id, nama, ipk);
+        dimas.print();
         //membuat Object
         //namaClass namaObject = new Constructor();
         Siswa sabrina = new Siswa();
